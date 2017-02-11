a68bfb402415981a857991cc13903ce42c95439a
cjr8920 <cjr8920@cs.rit.edu>
2017-02-11 16:43:41 -0500

    added lob functionality. Time to fine-tune

--- test.js
+++ test.js
@@ -3,27 +3,53 @@
 const ngrok = require('ngrok');
 const Fuse = require('fuse-email');
 const port = process.env.PORT || 3000;
+const Lob = require('lob-node')('test_87af502346326d11f66d8792d457c93ce26');
 
-ngrok.connect(port, function(err, url) {
- const fuse = Fuse({
-   email_key: '074cfb047cdf83b26b508cc4ed8c837ba142961d',
-   domain: url,
-   name: 'fishy',
-   sending_address: 'fishy@nathanpomana.com',
-   inbound_address: 'fishy@nathanpomana.com',
-   // restrict_inbound: false
- });
+ 
+	 ngrok.connect(port, function(err, url) {
+	 const fuse = Fuse({
+	   email_key: '074cfb047cdf83b26b508cc4ed8c837ba142961d',
+	   domain: url,
+	   name: 'fishy',
+	   sending_address: 'fishy@nathanpomana.com',
+	   inbound_address: 'fishy@nathanpomana.com',
+	 });
 
- fuse.setupTransport(function() {
-   fuse.setupServer(port, function(err, server) {
-     fuse.setupEndpoint(server);
-   });
- });
+	 fuse.setupTransport(function() {
+	   fuse.setupServer(port, function(err, server) {
+		 fuse.setupEndpoint(server);
+	   });
+	 });
+	 
 
- fuse.on('email_received', function(responder, inboundMessage) {
-   console.log(inboundMessage.subject);
-   responder.reply({
-     body: 'Hey! I got your email from an ngrok tunnel. Lit!'
-   });
- });
+	 fuse.on('email_received', function(responder, inboundMessage) {
+	   var text = inboundMessage;
+	   var subject = text.subject;
+	   subject = subject.split(",");
+	   Lob.letters.create({
+		   to: {
+			   name: subject[0],
+			   address_line1: subject[1],
+			   address_city: subject[2],
+			   address_state: subject[3],
+			   address_zip: subject[4],
+			   address_country: subject[5], 
+		   },
+		   from: {
+			   name: subject[0],
+			   address_line1: subject[1],
+			   address_city: subject[2],
+			   address_state: subject[3],
+			   address_zip: subject[4],
+			   address_country: subject[5],
+		   },
+		   file: text.html,
+		   color: true
+	   }, function(err, letter) {
+		  console.log(err, letter); 
+	   });
+	   responder.reply({
+		 body: 'Thank you for using Email-to-Amish. Your letter is being created and sent.'
+	   });
+	 });
 });
